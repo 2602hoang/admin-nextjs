8d912ff41471de08c0141a5e990221893e17b827
hoang2602 <vuhuyhoangboj@gmail.com>
2024-09-17 14:17:21 +0700

    fix deploy

---
+++ src/app/(Page)/auth/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <h1>Login or Sign Up</h1>
+      {/* Add your login form or content here */}
+    </div>
+  );
+}
+
+export default page;
