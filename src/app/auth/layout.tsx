ca6639e2f3c1638214a1a7e3ec576cda90166866
hoang2602 <vuhuyhoangboj@gmail.com>
2024-09-27 15:01:09 +0700

    fix

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout({
   return (
     <html lang="en">
       <body>
-        <div className="text-white bg-brown flex justify-center h-screen items-center w-full font-inter">
+        <div className="text-white bg-brown  fixed flex justify-center h-screen items-center w-full font-inter">
           {children}
         </div>
       </body>
